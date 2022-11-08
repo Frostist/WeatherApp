0ac26efecd9e466c0d1d6ee3a05bcf8feb5f0f71
Frostist <williamf1245@gmail.com>
2022-11-08 13:56:11 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Will Frost on 2022/11/08.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
